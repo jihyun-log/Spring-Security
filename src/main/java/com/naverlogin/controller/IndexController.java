6e1a5745a1cda876500a31d419f21e9f557ba31c
jihyun-log <o.ov0227@gmail.com>
2023-10-15 22:34:13 +0900

    SpringSecurity 설정

--- src/main/java/com/naverlogin/controller/IndexController.java
+++ src/main/java/com/naverlogin/controller/IndexController.java
@@ -2,6 +2,8 @@ package com.naverlogin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class IndexController {
@@ -10,4 +12,35 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join(){
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc(){
+        return "회원가입 완료됨";
+    }
+
 }
