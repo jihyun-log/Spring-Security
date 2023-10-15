9c3bb200a01d707ebfe18b0150d3c4cf0256563f
username <o.ov0227@gmail.com>
2023-10-15 09:23:02 +0900

    first commit

---
+++ src/main/java/com/naverlogin/NaverloginApplication.java
@@ -0,0 +1,13 @@
+package com.naverlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverloginApplication.class, args);
+	}
+
+}
