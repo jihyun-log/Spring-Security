0c24d4b1490db0736c902f94cf2265faba262686
jihyun-log <o.ov0227@gmail.com>
2023-10-16 23:29:00 +0900

    네이버 로그인 완료

--- src/main/java/com/naverlogin/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/naverlogin/config/oauth/PrincipalOauth2UserService.java
@@ -3,6 +3,7 @@ package com.naverlogin.config.oauth;
 import com.naverlogin.config.oauth.PrincipalDetails;
 import com.naverlogin.config.oauth.provider.FacebookUserInfo;
 import com.naverlogin.config.oauth.provider.GoogleUserInfo;
+import com.naverlogin.config.oauth.provider.NaverUserInfo;
 import com.naverlogin.config.oauth.provider.OAuth2UserInfo;
 import com.naverlogin.constant.Role;
 import com.naverlogin.entity.SocialUser;
@@ -14,6 +15,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
@@ -39,13 +42,19 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 
             oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
 
+        }else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")){
+            System.out.println("네이버 로그인 요청");
+
+            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
+
         }else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")){
             System.out.println("페이스북 로그인 요청");
 
             oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
 
-        }else{
-            System.out.println("구글과 페이스북만 지원");
+        }
+        else{
+            System.out.println("구글과 페이스북, 네이버만 지원");
         }
 
 
